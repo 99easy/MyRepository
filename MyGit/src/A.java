14e368c62bc8348c282540b6fe30c9c988297cf5
jimin <jimin@192.168.123.107>
2022-06-22 17:23:05 +0900

    First commit in devC branch

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,5 +3,7 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("This is A");
 		System.out.println("This is ??? version");
+		
+		System.out.println("devC branch");
 	}
 }
--- MyGit/src/C.java
+++ MyGit/src/C.java
@@ -2,5 +2,7 @@
 public class C {
 	public static void main(String args[]) {
 		System.out.println("This is C");
+		
+		System.out.println("devC branch");
 	}
 }
