6acd73d31c0459bcf4b3ffba900758c3488331aa
jimin <jimin@192.168.123.107>
2022-06-22 17:26:11 +0900

    Change in master after branching dev

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,5 +3,6 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("This is A");
 		System.out.println("This is ??? version");
+		System.out.println("Change in master branch");	
 	}
 }
